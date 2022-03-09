2b3e33cc490870696afa6f4ffba764f1da72037b
FilKys <million.fil@mail.ru>
2022-03-09 19:10:57 +0500

    Test
    Signed-off-by: FilKys <million.fil@mail.ru>

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_data_plot_from_file():
     with open('resources/US1.AAPL_210224_220224.csv') as file:
         data = csv.load(file)
         return data
-
+adsasd
 plt.title('График изменения цен акций')
 plt.xlable('Дата')
 plt.ylable('Цена')
