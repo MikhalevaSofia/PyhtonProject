e4b7a19d029b7ec93dab07c7b249aa6a6418b851
sofia2005 <mikhaleva.sofia2005@yandex.ru>
2022-03-18 21:30:57 +0500

    Init commit

--- main.py
+++ main.py
@@ -7,12 +7,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('resources/US1.AAPL_210224_220224.csv', sep=';',
-                 low_memory=False)
+df = pd.read_csv('resources/US1.AAPL_210224_220224.csv', sep=';')
 df['<CLOSE>'] = df['<CLOSE>'].astype(float)
 df['<DATE>'] = pd.to_datetime(arg=df['<DATE>'], infer_datetime_format=format('%d.%m.%Y'))
-plt.title('График изменения цен акций')
+plt.title('График изменения цен закрытия акций')
 plt.xlabel('Дата')
 plt.ylabel('Цена')
 plt.plot(df['<DATE>'], df['<CLOSE>'])
 plt.show()
+
+calc = pd.DataFrame()
+
+
+def calc_moving_average(x, days):
+    result = pd.DataFrame({'data'})
+    sum = 0.0
+    i = 0
+    for data in x['<CLOSE>'].values:
+        if i=days:
+
+
+#
+#     return x
+
+
+calc['7 days'] = calc_moving_average(df['<CLOSE>'], 7)
