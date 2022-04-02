016b7f5790d03c4bad5d6d18b200a5d7a09698cf
sofia2005 <mikhaleva.sofia2005@yandex.ru>
2022-04-02 21:31:41 +0500

    rsi

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ calc = pd.DataFrame()
 def calc_moving_average(x, days):
     sma = pd.DataFrame({'data': []})
     sum = 0.0
-    i = 0
+
     for i in range(0, days):
         sum = sum + x.loc[i]
-    print(sum)
+
     for i in range(0, x.values.size + 1 - days):
         sma.loc[i + days - 1] = sum / days
         if i <= x.values.size - days - 1:
@@ -39,7 +39,6 @@ def calc_moving_average(x, days):
 # TODO: ROC и MOM в 1 функцию
 def calc_momentum(y, days):
     mom = pd.DataFrame({'data': []})
-    i = 0
     for i in range(0, y.values.size + 1 - days):
         mom.loc[i + days - 1] = y.loc[i + days - 1] - y.loc[i]
     return mom
@@ -47,22 +46,61 @@ def calc_momentum(y, days):
 
 def calc_rate_of_change(z, days):
     roc = pd.DataFrame({'data': []})
-    i = 0
     for i in range(0, z.values.size + 1 - days):
         roc.loc[i + days - 1] = z.loc[i + days - 1] / z.loc[i] * 100
     return roc
 
+
+def calc_growth_for_rsi(w):
+    gr = pd.DataFrame({'data': []})
+    for i in range(0, w.values.size - 1):
+        gr.loc[i + 1] = w.loc[i + 1] - w.loc[i]
+    return gr
+
+
+def calc_relative_strength_index(m, days):
+    rsi = pd.DataFrame({'data': []})
+    PosSum = 0.0
+    NegSum = 0.0
+    for i in range(1, m.values.size + 2 - days):
+        l = i
+        for l in range(l, l + days):
+
+            if m.loc[i] > 0:
+                PosSum = PosSum + m.loc[i]
+            else:
+                if m.loc[i] < 0:
+                    NegSum = NegSum + m.loc[i]
+        grow = PosSum / abs(NegSum)
+        rsi.loc[i + days - 1] = 100 - (100 / (1 + grow))
+    return rsi
+
+
+# def calc_momentum(y, days):
+#     mr = pd.DataFrame({'mom': [], 'roc': []})
+#     i = 0
+#     for i in range(0, y.values.size + 1 - days):
+#         mom.loc[i + days - 1] = y.loc[i + days - 1] - y.loc[i]
+#         roc.loc[i + days - 1] = y.loc[i + days - 1] / y.loc[i] * 100
+#     return [mom, roc]
+
+
+calc['gr'] = calc_growth_for_rsi(df['<CLOSE>'])
+
+calc['rsi'] = calc_relative_strength_index(calc['gr'], 6)
+
 calc['7 days'] = calc_moving_average(df['<CLOSE>'], 7)
-print(calc)
 
 calc['14 days'] = calc_moving_average(df['<CLOSE>'], 14)
-print(calc)
 
 calc['21 days'] = calc_moving_average(df['<CLOSE>'], 21)
-print(calc)
 
 calc['mom'] = calc_momentum(df['<CLOSE>'], 7)
-print(calc)
 
 calc['roc'] = calc_rate_of_change(df['<CLOSE>'], 7)
+
 print(calc)
+
+# mr['mom'] = calc_momentum(df['<CLOSE>'], 7)
+# mr['roc'] = calc_momentum(df['<CLOSE>'], 7)
+# print(mr)
