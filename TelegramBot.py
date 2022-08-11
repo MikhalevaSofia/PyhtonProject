67a72cc877d40be41deb0958fe176b4c9df82e8b
sofia2005 <mikhaleva.sofia2005@yandex.ru>
2022-08-12 02:26:58 +0500

    На Сегодня Хватит

--- TelegramBot.py
+++ TelegramBot.py
@@ -9,8 +9,9 @@ def start(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	button1 = types.KeyboardButton('Тикеры MOEX/Tikers of MOEX')
 	button2 = types.KeyboardButton('Информация обо мне/Information about me')
+	button3 = types.KeyboardButton('Анекдот (опция только на русском)')
 
-	markup.add(button1, button2)
+	markup.add(button1, button2, button3)
 	bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user))
 	bot.send_message(message.chat.id,
 					 'Меня зовут Traider`s Assistant. Я твой бот-помощник в осуществлении торгов на Московской бирже. Для получения информации ты можешь воcпользоваться подсказками ниже.')
@@ -46,5 +47,11 @@ def bot_message(message):
 							 'Как я уже сказал, я буду помогать тебе торговать на московской бирже: покажу тебе изменение котировок, расскажу, когда лучше купить/продать твои акции, а также построю графики инструментов технического анализа, которые помогут тебе распознать достоверность отправленного мной сигнала')
 			bot.send_message(message.chat.id,
 							 'As I said, I will help you trade on the MOEX: I will show you the change in quotations, tell you when it is better to buy/ sell your shares, and also build graphs of technical analysis tools that will help you recognize the reliability of the signal I sent')
+		elif message.text == 'Анекдот (опция только на русском)':
+			bot.send_message(message.chat.id, 'Анекдот (опция только на русском)')
+			bot.send_message(message.chat.id, 'Беседуют две блондинки:'
+											  '— Как ты смогла при всех назвать меня дурой?'
+											  '— Извини, ты же не предупредила, что скрываешь.')
+
 
 bot.polling(none_stop=True)
