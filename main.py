370a418a05e35bcf6a92658ff2e88b7be7f59898
sofia2005 <mikhaleva.sofia2005@yandex.ru>
2022-08-11 22:40:06 +0500

    TelegramBot

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and set
 
 import apimoex
 import pandas as pd
