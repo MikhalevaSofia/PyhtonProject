76f3af1d9d5b167089f265575c0b9e42c7090b2f
sofia2005 <mikhaleva.sofia2005@yandex.ru>
2022-08-12 11:38:03 +0500

    Сделала кнопку 'Назад' в меню

--- TelegramBot.py
+++ TelegramBot.py
@@ -42,16 +42,35 @@ def bot_message(message):
 			markup.add(button1, button2, button3, button4, button5, button6, button7)
 			bot.send_message(message.chat.id, 'Тикеры MOEX/Tikers of MOEX', reply_markup=markup)
 		elif message.text == 'Информация обо мне/Information about me':
+			markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+			button1 = types.KeyboardButton('Назад/Back')
+			markup.add(button1)
 			bot.send_message(message.chat.id, 'Информация обо мне/Information about me')
 			bot.send_message(message.chat.id,
 							 'Как я уже сказал, я буду помогать тебе торговать на московской бирже: покажу тебе изменение котировок, расскажу, когда лучше купить/продать твои акции, а также построю графики инструментов технического анализа, которые помогут тебе распознать достоверность отправленного мной сигнала')
 			bot.send_message(message.chat.id,
-							 'As I said, I will help you trade on the MOEX: I will show you the change in quotations, tell you when it is better to buy/ sell your shares, and also build graphs of technical analysis tools that will help you recognize the reliability of the signal I sent')
+							 'As I said, I will help you trade on the MOEX: I will show you the change in quotations, tell you when it is better to buy/ sell your shares, and also build graphs of technical analysis tools that will help you recognize the reliability of the signal I sent',
+							 reply_markup=markup)
+
+
+
 		elif message.text == 'Анекдот (опция только на русском)':
+			markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+			button1 = types.KeyboardButton('Назад/Back')
+			markup.add(button1)
 			bot.send_message(message.chat.id, 'Анекдот (опция только на русском)')
 			bot.send_message(message.chat.id, 'Беседуют две блондинки:'
 											  '— Как ты смогла при всех назвать меня дурой?'
-											  '— Извини, ты же не предупредила, что скрываешь.')
+											  '— Извини, ты же не предупредила, что скрываешь.', reply_markup=markup)
+
+		elif message.text == 'Назад/Back':
+			markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+			button1 = types.KeyboardButton('Тикеры MOEX/Tikers of MOEX')
+			button2 = types.KeyboardButton('Информация обо мне/Information about me')
+			button3 = types.KeyboardButton('Анекдот (опция только на русском)')
+
+			markup.add(button1, button2, button3)
+			bot.send_message(message.chat.id, 'Назад/Back', reply_markup=markup)
 
 
 bot.polling(none_stop=True)
